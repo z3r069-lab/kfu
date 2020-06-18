10b9c49ff367d2885669703f063c95392f1874c2
z3r069-lab <55541381+z3r069-lab@users.noreply.github.com>
2020-06-18 21:33:52 +0300

    Add files via upload

---
+++ prog 4/prog 4/prog 4.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <ctime>
+#include "Head.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	Game game;
+	char character;
+	cout << "введите символ для игры (x | o)" << endl;
+	cin >> character;
+	while ((character != 'X') && (character != 'x') && (character != 'o') && (character != '0')) {
+		cout << "неверный символ попробуйте еще раз";
+	}
+	game = initGame(character);
+	updateDisplay(game);
+	while (!updateGame(&game))
+	{
+		game.isUserTurn == true ? userTurn(&game) : botTurn(&game);
+		updateDisplay(game);		
+	}
+	if (game.status == USER_WIN)
+	{
+		cout << "победа" << endl;
+	}
+	else if (game.status == BOT_WIN)
+	{
+		cout << "победил бот" << endl;
+	}
+	else
+	{
+		cout << "ничья" << endl;
+	}
+}
+
